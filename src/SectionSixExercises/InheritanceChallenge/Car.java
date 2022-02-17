1f9501925365dbc107a0656534228db05c584621
Simon Ekeroth <37657941+Warimano@users.noreply.github.com>
2022-02-17 15:47:03 +0100

    Add inheritance challenge

---
+++ src/SectionSixExercises/InheritanceChallenge/Car.java
@@ -0,0 +1,32 @@
+package SectionSixExercises.InheritanceChallenge;
+
+public class Car extends Vehicle {
+
+    private final String engineType;
+    private final int gears;
+    private int currentGear;
+
+    public Car(int gears, String engineType, int weight) {
+        super(weight);
+        this.engineType = engineType;
+        this.gears = Math.max(gears, 1);
+    }
+
+    public String getEngineType() {
+        return engineType;
+    }
+
+    public int getGears() {
+        return gears;
+    }
+
+    public int getCurrentGear() {
+        return currentGear;
+    }
+
+    public void setCurrentGear(int currentGear) {
+        if (currentGear < 1) {
+            this.currentGear = 1;
+        } else this.currentGear = Math.min(currentGear, this.gears);
+    }
+}
---
+++ src/SectionSixExercises/InheritanceChallenge/Vehicle.java
@@ -0,0 +1,22 @@
+package SectionSixExercises.InheritanceChallenge;
+
+public class Vehicle {
+    private int speed = 0;
+    private final int weight;
+
+    public Vehicle(int weight) {
+        this.weight = Math.max(weight, 1);
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+}
