2103c7819af310ee7deb446516411ca5517abf20
Simon Ekeroth <37657941+Warimano@users.noreply.github.com>
2022-02-19 08:55:05 +0100

    Add minimum element exercise #42

---
+++ src/com/warimano/SectionEightExercises/MinimumElement/MinimumElement.java
@@ -0,0 +1,32 @@
+package com.warimano.SectionEightExercises.MinimumElement;
+
+import java.util.Scanner;
+
+public class MinimumElement {
+
+    private static int readInteger() {
+        Scanner scanner = new Scanner(System.in);
+        int integer = scanner.nextInt();
+        scanner.close();
+        return integer;
+    }
+
+    private static int[] readElements(int elements) {
+        Scanner scanner = new Scanner(System.in);
+        int[] array = new int[elements];
+        for (int i = 0; i < elements; i++) {
+            array[i] = scanner.nextInt();
+        }
+        scanner.close();
+        return array;
+    }
+
+    private static int findMin(int[] array) {
+        int min = Integer.MAX_VALUE;
+        for (int i : array) {
+            min = Math.min(min, i);
+        }
+        return min;
+    }
+
+}
