5702f719e5dda86934cd95e54f11954c9396cb91
Simon Ekeroth <37657941+Warimano@users.noreply.github.com>
2022-02-19 09:08:50 +0100

    Add reverse array exercise #43

---
+++ src/com/warimano/SectionEightExercises/ReverseArray/ReverseArray.java
@@ -0,0 +1,16 @@
+package com.warimano.SectionEightExercises.ReverseArray;
+
+
+import java.util.Arrays;
+
+public class ReverseArray {
+    private static void reverse(int[] array) {
+        System.out.println("Array = " + Arrays.toString(array));
+        for (int i = 0; i < array.length / 2; i++) {
+            int temp = array[i];
+            array[i] = array[array.length - i - 1];
+            array[array.length - i - 1] = temp;
+        }
+        System.out.println("Reversed array = " + Arrays.toString(array));
+    }
+}
