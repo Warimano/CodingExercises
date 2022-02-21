2bc4ff86e5ab0f29c80bf091cd4721f2d2e9276c
Simon Ekeroth <37657941+Warimano@users.noreply.github.com>
2022-02-21 15:23:15 +0100

    Add Playlist exercise #46

---
+++ src/com/warimano/SectionEightExercises/Playlist/Album.java
@@ -0,0 +1,43 @@
+package com.warimano.SectionEightExercises.Playlist;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Objects;
+
+public class Album {
+    private String name, artist;
+    private ArrayList<Song> songs;
+
+    public Album(String name, String artist) {
+        this.name = name;
+        this.artist = artist;
+        this.songs = new ArrayList<>();
+    }
+
+    public boolean addSong(String title, double duration) {
+        if (findSong(title) != null) return false;
+
+        return songs.add(new Song(title, duration));
+    }
+
+    private Song findSong(String title) {
+        for (Song song : songs) {
+            if (Objects.equals(song.getTitle(), title)) {
+                return song;
+            }
+        }
+        return null;
+    }
+
+    public boolean addToPlayList(int trackNumberInAlbum, LinkedList<Song> playlist) {
+        if (songs.size() < trackNumberInAlbum || trackNumberInAlbum < 1) return false;
+        return playlist.add(songs.get(trackNumberInAlbum - 1));
+    }
+
+    public boolean addToPlayList(String title, LinkedList<Song> playlist) {
+        Song song = findSong(title);
+        if (song == null) return false;
+
+        return playlist.add(song);
+    }
+}
